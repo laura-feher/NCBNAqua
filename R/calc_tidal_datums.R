4928d9fa17dbdea2158553af7c6b0d972aeb0032
Feher <lfeher@nps.gov>
2025-09-29 15:51:05 -0400

    ⚡️🎨added support for user-supplied 'group_by' groups and fixes get_wl_data to work with GWMP data

--- R/calc_tidal_datums.R
+++ R/calc_tidal_datums.R
@@ -1,35 +1,18 @@
-calc_tidal_datums <- function(wl_data, window = 11.5) {
-  find_extremes <- function(x, fun, window = window) {
-    window_size <- window*4
-    half_window <- floor(window_size / 2)
-    roll_extreme <- zoo::rollapply(x, width = window_size, FUN = fun, fill = NA, align = "center")
-    is_extreme <- x == roll_extreme
-    # Remove flat peaks/troughs by ensuring unique max/min in the window
-    is_extreme <- purrr::map_lgl(seq_along(x), function(i) {
-      if (is.na(is_extreme[i]) || !is_extreme[i]) return(FALSE)
-      window_start <- max(1, i - half_window)
-      window_end <- min(length(x), i + half_window)
-      window_vals <- x[window_start:window_end]
-      center_index <- i - window_start + 1
-      # Check if the extreme value is unique in the window
-      sum(window_vals == x[i]) == 1
-    })
-    ifelse(is_extreme, x, NA_real_)
-  }
+calc_tidal_datums <- function(wl_data) {
   
   # Find high and low extremes
   df_extremes <- wl_data %>%
-    group_by(Identifier, Name, LocationIdentifier) %>%
-    nest(data = -c(Identifier, Name, LocationIdentifier)) %>%
-    mutate(highs = map(data, ~find_extremes(.$water_level, max)),
-           lows = map(data, ~find_extremes(.$water_level, min))) %>%
+    group_by(Identifier, Name, LocationIdentifier, .add = TRUE) %>%
+    nest() %>%
+    mutate(highs = map(data, ~find_extremes(.x$water_level, max)),
+           lows = map(data, ~find_extremes(.x$water_level, min))) %>%
     unnest(cols = c(data, highs, lows)) %>%
     mutate(date_chr = lubridate::as_date(date))
   
   # Calculate MHW and MHHW from the extremes
   highs <- df_extremes %>%
     filter(!is.na(highs)) %>%
-    group_by(Identifier, Name, LocationIdentifier) %>%
+    group_by(Identifier, Name, LocationIdentifier, .add = TRUE) %>%
     nest() %>%
     mutate(MHW = map_dbl(data, ~mean(.x$highs, na.rm = TRUE)),
            MHHW = map_dbl(data, ~.x %>%
@@ -44,7 +27,7 @@ calc_tidal_datums <- function(wl_data, window = 11.5) {
   # Calculate MLW and MLLW from the extremes
   lows <- df_extremes %>%
     filter(!is.na(lows)) %>%
-    group_by(Identifier, Name, LocationIdentifier) %>%
+    group_by(Identifier, Name, LocationIdentifier, .add = TRUE) %>%
     nest() %>%
     mutate(MLW = map_dbl(data, ~mean(.x$lows, na.rm = TRUE)),
            MLLW = map_dbl(data, ~.x %>%
@@ -56,7 +39,7 @@ calc_tidal_datums <- function(wl_data, window = 11.5) {
            )) %>%
     select(-data)
   
-  tidal_datums <- full_join(highs, lows, by = join_by(Name, Identifier, LocationIdentifier))
+  tidal_datums <- full_join(highs, lows)
   
   return(tidal_datums)
 }
